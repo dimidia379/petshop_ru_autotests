3fdafdb702b7dec84d106bd249e8a6cb9e65631c
dimidia379 <j.zvereva050@gmail.com>
2021-12-22 20:59:20 +0300

    Remove credentials

--- src/test/java/cloud/autotests/tests/TestBase.java
+++ src/test/java/cloud/autotests/tests/TestBase.java
@@ -1,53 +1,30 @@
 package cloud.autotests.tests;
 
-import cloud.autotests.config.CredentialsConfig;
 import cloud.autotests.config.Project;
 import cloud.autotests.helpers.AllureAttachments;
 import cloud.autotests.helpers.DriverSettings;
 import cloud.autotests.helpers.DriverUtils;
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.junit5.AllureJunit5;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static cloud.autotests.tests.TestData.PRODUCT_URL;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
-import static java.lang.String.format;
 
 
 @ExtendWith({AllureJunit5.class})
 public class TestBase {
-    public static CredentialsConfig credentials =
-            ConfigFactory.create(CredentialsConfig.class);
 
     @BeforeAll
     static void setUp() {
-
-
-        Configuration.browserSize = "1920x1080";
-
-        String url = System.getProperty("url", "selenoid.autotests.cloud/wd/hub/");
-        String login = credentials.login();
-        String password = credentials.password();
-        Configuration.remote = format("https://%s:%s@%s", login, password, url);
-
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         DriverSettings.configure();
-
-
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("enableVNC", true);
-//        capabilities.setCapability("enableVideo", true);
-//
-//        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
